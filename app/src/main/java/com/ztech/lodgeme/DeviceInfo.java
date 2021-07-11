3399419f9cc98eca59f8772f6f2a63d3d3ab431e
unknown <zraelwalker@gmail.com>
2021-07-11 02:45:16 +0100

    Created a Property Listing Application in Java Android

---
+++ app/src/main/java/com/ztech/lodgeme/DeviceInfo.java
@@ -0,0 +1,12 @@
+package com.ztech.lodgeme;
+
+import java.io.Serializable;
+
+public class DeviceInfo implements Serializable {
+
+    public String device;
+    public String os_version;
+    public String serial;
+    public String regid;
+
+}
